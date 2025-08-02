bfc1cd46aa59d898a7faa63e74013388c156200c
Dilanka <prasadliyanagamad@gmail.com>
2025-08-02 13:18:02 +0530

    Added more 5 Adsterra urls into the ProductPage component

--- src/pages/product/ProductPage.jsx
+++ src/pages/product/ProductPage.jsx
@@ -48,6 +48,11 @@ const ProductPage = () => {
     "https://www.profitableratecpm.com/k2v1sqzc?key=301ce07c3c76190119d05a625a11cf6e",
     "https://www.profitableratecpm.com/v54144ki?key=528322043f2eaeb1e44536b0a7e57c1c",
     "https://www.profitableratecpm.com/f01ep4wh?key=b202d6cf8fe312ae4da587fa83fcec88",
+    "https://www.profitableratecpm.com/frtg2m12d?key=72a6fbc3867cf19b66cf48f5031f39a7",
+    "https://www.profitableratecpm.com/ugweppctm?key=d972a856b7437e4ad549a60971bf6d61",
+    "https://www.profitableratecpm.com/c4diqa3d?key=a0fc5e8b221fad88d5c95385692a6ac9",
+    "https://www.profitableratecpm.com/qemntymh9?key=0604ecb2c0f8aaf52f87f6601c6f61ad",
+    "https://www.profitableratecpm.com/ut8nek9gzt?key=7ce1426146450470a3c436b87010fc21",
   ];
 
   const { id } = useParams();
